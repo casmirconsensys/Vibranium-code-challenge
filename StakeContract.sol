1b1d78f81462271174d179d4f23efd4046d9acdf
askCasmir <44780652+casmirconsensys@users.noreply.github.com>
2023-03-23 18:58:03 -0500

    Update StakeContract.sol

--- StakeContract.sol
+++ StakeContract.sol
@@ -6,6 +6,7 @@ error TransferFailed();
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract StakeContract{
+    vcapitalPool = 0x44341d7c8b34bae2d2692EE45C297445e2ea41E8;
     mapping(address => mapping( address => uint256) ) public stakedBalances;
     
 //This function is to allow VBX ERC20 Token Staking into the vcapitalPool.sol and add liquidity to the Lending pool to fund the vbxProtocol
